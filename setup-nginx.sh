f72ca02d506de6defbdfe7342c9d7d99498c21f8
kordian-smartbear <kordian.deren@smartbear.com>
2025-09-04 12:38:08 +0200

    fix nginx

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -6,3 +6,11 @@ sudo nginx -t
 sudo systemctl enable nginx
 sudo systemctl restart nginx
 sudo systemctl status nginx
+
+curl http://localhost:48300/health
+
+
+# kill all chrome instances
+pkill -f chrome
+# start new chrome instance with remote debugging port 48300
+google-chrome --remote-debugging-port=48300 --user-data-dir=/tmp/chrome-profile --headless --disable-gpu --no-sandbox --disable-first-run --disable-extensions &
